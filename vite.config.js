c454b82cebd751c39e7cc1cf5431df7c53c6ec4f
Ilya Shusterman <shusterilyaman@gmail.com>
2024-08-19 15:59:23 +0300

    fix: update vite version to ^5.4.1 and add optional dependencies for sass-embedded in package-lock.json and package.json, also update vite.config.js to reflect the new version.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/fast-evals/", // Replace with your repository name
   plugins: [react()],
+  base: "/fast-evals/",
 });
