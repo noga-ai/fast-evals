318f4f9e3e86a04924c39048ea6e09238adc5602
Ilya Shusterman <shusterilyaman@gmail.com>
2024-08-19 16:06:51 +0300

    fix: configure vite to handle different commands

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react()],
-  base: "/fast-evals/",
+export default defineConfig(({ command }) => {
+  return {
+    base: command === "build" ? "/fast-evals/" : "/",
+    plugins: [react()],
+  };
 });
